51383b88fe872baa56c40a25e5782d85b94e946f
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2017-10-20 21:57:41 +0530

    basic logger setup

---
+++ Baray.js
@@ -0,0 +1,63 @@
+const fs = require('fs')
+
+class Baray {
+    constructor(options) {
+        this.appName = options.appName || "NoName"
+        this.console = options.console && true
+        this.rotateLogs = options.rotateLogs ? options.rotateLogs : false
+        this.json = options.json && true
+        this.path = options.path || `${__dirname}/logs`
+        if (!fs.existsSync(this.path)) {
+            fs.mkdirSync(this.path);
+        }
+        this.BarayConsole = new console.Console(this.getFile(types.INFO), this.getFile(types.ERR))
+    }
+    info(message) {
+        const logEntry = new LogModel(this.appName, message, types.INFO)
+        this.print(types.INFO, logEntry)
+    }
+    error(message) {
+        const logEntry = new LogModel(this.appName, message, types.ERR)
+        this.print(types.ERR, logEntry)
+    }
+    print(type, content) {
+        // put condition to rotate log based on date here
+        switch (type) {
+            case types.INFO:
+                this.BarayConsole.log(JSON.stringify(content))
+                if (this.console)
+                    console.log(JSON.stringify(content))
+                break
+            case types.ERR:
+                this.BarayConsole.log(JSON.stringify(content))
+                if (this.console)
+                    console.log(JSON.stringify(content))
+                break
+            default:
+                console.log(JSON.stringify(content))
+        }
+    }
+    getFile(type) {
+        return fs.createWriteStream(this.getFilePath(type), {
+            'flags': 'a'
+        })
+    }
+    getFilePath(type) {
+        return `${this.path}/${type}.log`
+    }
+}
+
+class LogModel {
+    constructor(appName, message, type) {
+        this.appName = appName
+        this.type = type
+        this.timestamp = new Date()
+        this.message = message
+    }
+}
+const types = {
+    INFO: "info",
+    ERR: "warning"
+}
+
+module.exports = Baray
---
+++ test.js
@@ -0,0 +1,39 @@
+const baray = require('./Baray')
+const winston = require('winston')
+
+const logger = new baray({
+    appName: "test",
+    console: true,
+    rotateLogs: false,
+    json: true
+})
+
+// For comparision purposes 
+const loggerW = new(winston.Logger)({
+    transports: [
+        new(winston.transports.File)({
+            name: 'info-file',
+            filename: `logs/winston-info.log`,
+            level: 'info'
+        }),
+        new(winston.transports.File)({
+            name: 'error-file',
+            filename: `logs/winston-error.log`,
+            level: 'error'
+        })
+    ]
+})
+
+const msg = 'test log message'
+const msgJson = {
+    statusCode: 400,
+    data: "something..."
+}
+
+// for (let i = 0; i < 1e6; i++)
+//     logger.info(msg)
+
+loggerW.error(msg)
+
+logger.info(msg)
+logger.error(msg)
