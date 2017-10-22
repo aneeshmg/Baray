bc01e7c89127842617c5caa5f62c19183c15aab2
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2017-10-22 20:29:22 +0530

    add logger initialization tests

--- tests/baray.spec.js
+++ tests/baray.spec.js
@@ -23,4 +23,31 @@ describe("Baray tests", () => {
             expect(logger).to.haveOwnProperty("path")
         })
     })
+    describe("Initialization via config tests", () => {
+        let logger = null
+        const appName = "test"
+        const _console = false // added the "_" as just 'console' is a reserved word
+        const json = true
+        const path = `${__dirname}/logs`
+        before(() => {
+            logger = new baray({
+                appName: appName,
+                console: _console,
+                json: json,
+                path: path
+            })
+        })
+        it("should set appName", () => {
+            expect(logger).to.haveOwnProperty("appName").to.equal(appName)
+        })
+        it("should set 'console'", () => {
+            expect(logger).to.haveOwnProperty("console").to.equal(_console)
+        })
+        it("should set json option", () => {
+            expect(logger).to.haveOwnProperty("json").to.equal(json)
+        })
+        it("should set log path", () => {
+            expect(logger).to.haveOwnProperty("path").to.equal(path)
+        })
+    })
 })
